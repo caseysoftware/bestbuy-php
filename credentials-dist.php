515579393cc60d270e6e679c47a4e70e99a2c6ca
Keith Casey <keith@caseysoftware.com>
2014-10-29 22:15:01 -0500

    adding creeds

---
+++ credentials-dist.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * To use these examples, you have to rename this file to credentials.php and then fill in your API Key. It's
+ *   available from here: https://remix.mashery.com/apps/mykeys
+ */
+
+$apikey = 'asdf1234';
