53ef08e178232e5ae630759e782b95f49ea2bf45
Keith Casey <keith@caseysoftware.com>
2014-10-31 08:17:47 -0500

    refactoring out the different bits

--- src/BestBuy/Products.php
+++ src/BestBuy/Products.php
@@ -4,21 +4,23 @@ namespace BestBuy;
 
 class Products extends Resource
 {
+    protected $resource = 'products';
+
     public function index($page = 1, $pagesize = 10)
     {
-        return $this->client->get('products', array('pageSize' => $pagesize, 'page' => $page));
+        return $this->client->get($this->resource, array('pageSize' => $pagesize, 'page' => $page));
     }
 
     public function load($product_id)
     {
         $data = $this->client->get('products(productId=' . $product_id . ')');
 
-        if (!isset($data['products'])) {
+        if (!isset($data[$this->resource])) {
             // todo: this only happens on an error.. should we throw an exception?
             return $data;
         }
-        if (isset($data['products'][0])) {
-            return $data['products'][0];
+        if (isset($data[$this->resource][0])) {
+            return $data[$this->resource][0];
         } else {
             return array();
         }
--- src/BestBuy/Stores.php
+++ src/BestBuy/Stores.php
@@ -4,21 +4,23 @@ namespace BestBuy;
 
 class Stores extends Resource
 {
+    protected $resource = 'stores';
+
     public function index($page = 1, $pagesize = 10)
     {
-        return $this->client->get('stores', array('pageSize' => $pagesize, 'page' => $page));
+        return $this->client->get($this->resource, array('pageSize' => $pagesize, 'page' => $page));
     }
 
     public function load($store_id)
     {
         $data = $this->client->get('stores(storeId=' . $store_id . ')');
 
-        if (!isset($data['stores'])) {
+        if (!isset($data[$this->resource])) {
             // todo: this only happens on an error.. should we throw an exception?
             return $data;
         }
-        if (isset($data['stores'][0])) {
-            return $data['stores'][0];
+        if (isset($data[$this->resource][0])) {
+            return $data[$this->resource][0];
         } else {
             return array();
         }
