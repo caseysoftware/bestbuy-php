f7fe04e7ca50519f02947dc4285f97de264760da
Keith Casey <keith@caseysoftware.com>
2014-10-30 21:59:11 -0500

    wired pagination for the products

--- src/BestBuy/Products.php
+++ src/BestBuy/Products.php
@@ -4,8 +4,8 @@ namespace BestBuy;
 
 class Products extends Resource
 {
-    public function index()
+    public function index($page = 1, $pagesize = 10)
     {
-        return $this->client->get('products', array());
+        return $this->client->get('products', array('pageSize' => $pagesize, 'page' => $page));
     }
 }
