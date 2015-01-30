56e05480bf99a5ded0cf7b8928ef1acba31962e7
Keith Casey <keith@caseysoftware.com>
2015-01-30 00:06:29 -0600

    implemented city search

--- src/BestBuy/Stores.php
+++ src/BestBuy/Stores.php
@@ -6,4 +6,13 @@ class Stores extends \BestBuy\Resources\Base
 {
     protected $resource = 'stores';
     protected $resourceId = 'storeId';
+
+    public function byCity($city)
+    {
+        $result = $this->client->get($this->resource . '(city=' . $city . ')');
+        $this->data = $result[$this->resource];
+
+        return $this;
+    }
+
 }
